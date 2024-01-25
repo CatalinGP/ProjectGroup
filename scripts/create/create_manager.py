5a428690bedf6adcf4761fe3326c1018a102ba1f
CatalinGP <catalin.popescu@randstaddigital.com>
2024-01-25 15:00:59 +0200

    modified:   scripts/create/create_manager.py

--- scripts/create/create_manager.py
+++ scripts/create/create_manager.py
@@ -11,27 +11,25 @@ filename = "ubuntu-22.04.3-live-server-amd64.iso"
 relative_directory = "tmp/downloaded_utils"
 
 
-def check_iso_file(url, filename, relative_directory):
+def check_iso_file(url_path, file_name, rel_directory):
     script_directory = os.path.dirname(os.path.abspath(__file__))
 
-    full_path_dir = os.path.join(script_directory, "..", "..", relative_directory, filename)
+    full_path_dir = os.path.join(script_directory, "..", "..", rel_directory, file_name)
 
-    # Check if the file already exists
     if os.path.exists(full_path_dir):
-        logger.info(f"File '{filename}' already exists in the specified directory.")
+        logger.info(f"File '{file_name}' already exists in the specified directory.")
     else:
         try:
-            # Ensure the necessary directory structure exists
-            os.makedirs(os.path.join(script_directory, "..", "..", relative_directory), exist_ok=True)
+            os.makedirs(os.path.join(script_directory, "..", "..", rel_directory), exist_ok=True)
 
-            logger.info(f"Downloading '{filename}'...")
+            logger.info(f"Downloading '{file_name}'...")
 
-            # Download the file using urllib and shutil
-            with urlopen(url) as response, open(full_path_dir, 'wb') as output_file:
+            with urlopen(url_path) as response, open(full_path_dir, 'wb') as output_file:
                 shutil.copyfileobj(response, output_file)
 
             logger.info(f"File downloaded and saved to {full_path_dir}")
         except Exception as e:
             logger.info(f"Error: {e}")
+
 
 check_iso_file(url, filename, relative_directory)
